80785b9f5afa28b6c4b0ee003d11b1df6f63d648
Muhammad Usman <57209319+mUsman3@users.noreply.github.com>
2020-03-12 20:44:14 +0500

    Calculator app using ifis is done

---
+++ Calculator-App-Using-ifis/assets/scripts/app.js
@@ -0,0 +1,72 @@
+const defaultResult = 0;
+let currentResult = defaultResult;
+let logEntries = [];
+
+// Gets input from input field
+function getUserNumberInput() {
+  return parseInt(usrInput.value);
+}
+
+// Generates and writes calculation log
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+  const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+  outputResult(currentResult, calcDescription); // from vendor file
+}
+
+function writeToLog(
+  operationIdentifier,
+  prevResult,
+  operationNumber,
+  newResult
+) {
+  const logEntry = {
+    operation: operationIdentifier,
+    prevResult: prevResult,
+    number: operationNumber,
+    result: newResult
+  };
+  logEntries.push(logEntry);
+  console.log(logEntries);
+}
+
+function CalculateResult(CalcType) {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  let mathOperator;
+
+  if (CalcType === "ADD") {
+    currentResult += enteredNumber;
+    mathOperator = "+";
+  } else if (CalcType === "SUB") {
+    currentResult -= enteredNumber;
+    mathOperator = "-";
+  } else if (CalcType === "MUL") {
+    currentResult *= enteredNumber;
+    mathOperator = "*";
+  } else if (CalcType === "DIV") {
+    currentResult /= enteredNumber;
+    mathOperator = "/";
+  } else {
+    console.log("unknown operator");
+  }
+  createAndWriteOutput(mathOperator, initialResult, enteredNumber);
+  writeToLog(CalculateResult, initialResult, enteredNumber, currentResult);
+}
+
+function add() {
+  CalculateResult("ADD");
+}
+function subtract() {
+  CalculateResult("SUB");
+}
+function multiply() {
+  CalculateResult("MUL");
+}
+function divide() {
+  CalculateResult("DIV");
+}
+
+addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", subtract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
