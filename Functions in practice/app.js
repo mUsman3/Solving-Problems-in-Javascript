7ec9e197c31d1f0bff2c79fab4ec3efdca881718
Muhammad Usman <usman59228@gmail.com>
2020-03-15 12:53:29 +0500

    Started working on game for practcing functions

--- Functions in practice/app.js
+++ Functions in practice/app.js
@@ -1,19 +1,28 @@
 const startGameBtn = document.getElementById("start-game-btn");
 
-const func = function() {
-  console.log("my name is usman");
-};
+const ROCK = "ROCK";
+const PAPER = "PAPER";
+const SCISSOR = "SCISSOR";
+const DEFAULT = "PAPER";
 
-func();
-// myFun();
-
-const person = {
-  fun: function myFunction() {
-    console.log("my name is usman also");
+const Game = function() {
+  const selectOption = prompt(
+    `${ROCK}, ${PAPER} or ${SCISSOR}`,
+    ""
+  ).toUpperCase();
+  if (
+    selectOption !== ROCK &&
+    selectOption !== PAPER &&
+    selectOption !== SCISSOR
+  ) {
+    alert(`we have choose ${DEFAULT}, as you made invalid input`);
+    return DEFAULT;
   }
+  return selectOption;
 };
 
-// person.fun();
-
-startGameBtn.addEventListener("click", func);
-startGameBtn.addEventListener("click", func);
+startGameBtn.addEventListener("click", function() {
+  console.log("Game Starting");
+  const selctedValue = Game();
+  console.log(selctedValue);
+});
