30cca97a8558559c2773d10e6d237524c48e8718
Muhammad Usman <usman59228@gmail.com>
2020-03-15 13:54:03 +0500

    features added in game using ES5 Javascript

--- Functions in practice/app.js
+++ Functions in practice/app.js
@@ -4,8 +4,9 @@ const ROCK = "ROCK";
 const PAPER = "PAPER";
 const SCISSOR = "SCISSOR";
 const DEFAULT = "PAPER";
+let gameRunning = false;
 
-const Game = function() {
+const playerChoice = function() {
   const selectOption = prompt(
     `${ROCK}, ${PAPER} or ${SCISSOR}`,
     ""
@@ -21,8 +22,40 @@ const Game = function() {
   return selectOption;
 };
 
+const computerChoice = function() {
+  const randomValue = Math.random();
+  if (randomValue < 0.33) {
+    return PAPER;
+  } else if (randomValue < 0.66) {
+    return SCISSOR;
+  } else {
+    return PAPER;
+  }
+};
+
+const winner = function(compChoice, plyrChoice) {
+  if (compChoice === plyrChoice) {
+    return "Match is Draw";
+  } else if (
+    (compChoice == ROCK && plyrChoice == PAPER) ||
+    (compChoice == PAPER && plyrChoice == SCISSOR)
+  ) {
+    return "Player win";
+  } else {
+    return "Computer Win";
+  }
+};
+
 startGameBtn.addEventListener("click", function() {
+  if (gameRunning) {
+    return;
+  }
+  gameRunning;
   console.log("Game Starting");
-  const selctedValue = Game();
-  console.log(selctedValue);
+  // const selctedValue = Game();
+  // console.log(selctedValue);
+  const playChoice = playerChoice();
+  const ComputerChoice = computerChoice();
+  const Winner = winner(ComputerChoice, playChoice);
+  console.log(Winner);
 });
