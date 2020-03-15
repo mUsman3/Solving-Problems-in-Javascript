0f7abb9941fe01334c77b7e50763ece607861e5a
Muhammad Usman <57209319+mUsman3@users.noreply.github.com>
2020-03-15 12:05:57 +0500

    Practicing basics

--- Functions in practice/app.js
+++ Functions in practice/app.js
@@ -1,1 +1,18 @@
-const startGameBtn = document.getElementById('start-game-btn');
+const startGameBtn = document.getElementById("start-game-btn");
+
+const func = function() {
+  console.log("my name is usman");
+};
+
+func();
+// myFun();
+
+const person = {
+  fun: function myFunction() {
+    console.log("my name is usman also");
+  }
+};
+
+// person.fun();
+
+startGameBtn.addEventListener("click", func);
