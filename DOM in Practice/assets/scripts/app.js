50475b51596819bc87020fd95a23f995ee83b399
Muhammad Usman <usman59228@gmail.com>
2020-03-24 11:05:42 +0500

    Add Fav movie Project, DOM in Practice

---
+++ DOM in Practice/assets/scripts/app.js
@@ -0,0 +1,5 @@
+const addMovieModal = document.getElementById('add-modal');
+// const addMovieModal = document.querySelector('#add-modal');
+// const addMovieModal = document.body.children[1];
+const startAddMovieButton = document.querySelector('header button');
+// const startAddMovieButton = document.querySelector('header').lastElementChild;
