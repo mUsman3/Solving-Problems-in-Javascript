cd096e4f25ad1647674201806567e339c4afd5a1
Muhammad Usman <usman59228@gmail.com>
2020-03-24 12:27:09 +0500

    Add movie button, cancel button is active now

--- DOM in Practice/assets/scripts/app.js
+++ DOM in Practice/assets/scripts/app.js
@@ -1,5 +1,27 @@
-const addMovieModal = document.getElementById('add-modal');
+const addMovieModal = document.getElementById("add-modal");
 // const addMovieModal = document.querySelector('#add-modal');
 // const addMovieModal = document.body.children[1];
-const startAddMovieButton = document.querySelector('header button');
+const startAddMovieButton = document.querySelector("header button");
 // const startAddMovieButton = document.querySelector('header').lastElementChild;
+const backdrop = document.getElementById("backdrop");
+const cancelButtonModal = addMovieModal.querySelector(".btn--passive");
+
+
+//functions
+const toggleBackdrop = () => {
+  backdrop.classList.toggle("visible");
+};
+
+const toggleButton = () => {
+  addMovieModal.classList.toggle("visible");
+  toggleBackdrop();
+};
+
+const cancelButtonHandler = () => {
+  toggleButton();
+};
+
+
+startAddMovieButton.addEventListener("click", toggleButton);
+backdrop.addEventListener("click", toggleButton);
+cancelButtonModal.addEventListener("click", cancelButtonHandler);
