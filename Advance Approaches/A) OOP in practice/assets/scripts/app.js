5662997f8fe8f607899d2855f608038f49034971
Muhammad Usman <usman59228@gmail.com>
2020-04-01 13:18:42 +0500

    One step Ahead, Static Method used

--- Advance Approaches/A) OOP in practice/assets/scripts/app.js
+++ Advance Approaches/A) OOP in practice/assets/scripts/app.js
@@ -1,14 +1,34 @@
 class Product {
-  title;
-  imgUrl;
-  price;
+  title = "DEFAULT";
+  imageUrl;
   description;
+  price;
 
-  constructor(title, imgUrl, price, desc) {
-    (this.title = title),
-      (this.imgUrl = imgUrl),
-      (this.price = price),
-      (this.description = desc);
+  constructor(title, image, desc, price) {
+    this.title = title;
+    this.imageUrl = image;
+    this.description = desc;
+    this.price = price;
+  }
+}
+
+class ShoppingCart {
+  items = [];
+
+  addProduct(product) {
+    this.items.push(product);
+    this.totalOutput.innerHTML = `<h2>Total: \$${1}</h2>`;
+  }
+
+  render() {
+    const cartEl = document.createElement("section");
+    cartEl.innerHTML = `
+      <h2>Total: \$${0}</h2>
+      <button>Order Now!</button>
+    `;
+    cartEl.className = "cart";
+    this.totalOutput = cartEl.querySelector("h2");
+    return cartEl;
   }
 }
 
@@ -16,28 +36,28 @@ class ProductItem {
   constructor(product) {
     this.product = product;
   }
-  addtoCart() {
-    console.log("Adding To cart");
-    console.log(this);
+
+  addToCart() {
+    App.addProductToCart(this.product);
   }
 
   render() {
-    const productEl = document.createElement("li");
-    productEl.className = "product-item";
-    productEl.innerHTML = `
-    <div>
-      <img src= ${this.product.imgUrl} alt="${this.product.title}" >
-        <div class="product-item__content">
-         <h2>${this.product.title}</h2>
-         <h3>\$${this.product.price}</h3>
-         <p>${this.product.description}</p>
-         <button>Add to cart</button>
+    const prodEl = document.createElement("li");
+    prodEl.className = "product-item";
+    prodEl.innerHTML = `
+        <div>
+          <img src="${this.product.imageUrl}" alt="${this.product.title}" >
+          <div class="product-item__content">
+            <h2>${this.product.title}</h2>
+            <h3>\$${this.product.price}</h3>
+            <p>${this.product.description}</p>
+            <button>Add to Cart</button>
+          </div>
         </div>
-    </div>
-    `;
-    const addtoCartBtn = productEl.querySelector("button");
-    addtoCartBtn.addEventListener("click", this.addtoCart.bind(this));
-    return productEl;
+      `;
+    const addCartButton = prodEl.querySelector("button");
+    addCartButton.addEventListener("click", this.addToCart.bind(this));
+    return prodEl;
   }
 }
 
@@ -57,18 +77,45 @@ class ProductList {
     )
   ];
   constructor() {}
+
   render() {
-    const renderHook = document.getElementById("app");
     const prodList = document.createElement("ul");
     prodList.className = "product-list";
     for (const prod of this.products) {
       const productItem = new ProductItem(prod);
-      const productEl = productItem.render();
-      prodList.append(productEl);
+      const prodEl = productItem.render();
+      prodList.append(prodEl);
     }
-    renderHook.append(prodList);
+    return prodList;
   }
 }
 
-const productList = new ProductList();
-productList.render();
+class Shop {
+  render() {
+    const renderHook = document.getElementById("app");
+
+    this.cart = new ShoppingCart();
+    const cartEl = this.cart.render();
+    const productList = new ProductList();
+    const prodListEl = productList.render();
+
+    renderHook.append(cartEl);
+    renderHook.append(prodListEl);
+  }
+}
+
+class App {
+  static cart;
+
+  static init() {
+    const shop = new Shop();
+    shop.render();
+    this.cart = shop.cart;
+  }
+
+  static addProductToCart(product) {
+    this.cart.addProduct(product);
+  }
+}
+
+App.init();
